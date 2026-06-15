482ec6510fce9ab8dac6c39adaa83db20720be7c
Ruban.M <dkrkumar8585@gmail.com>
2026-06-15 12:36:11 +0530

    Create Q9.py

---
+++ AMAZON_PROBLEMS/Q9.py
@@ -0,0 +1,21 @@
+def check_leaves_same_level(root):
+    if not root: return True
+    
+    leaf_level = None
+    
+    def dfs(node, level):
+        nonlocal leaf_level
+        if not node:
+            return True
+        
+        # It's a leaf
+        if not node.left and not node.right:
+            if leaf_level is None:
+                leaf_level = level
+            elif leaf_level != level:
+                return False
+            return True
+        
+        return dfs(node.left, level + 1) and dfs(node.right, level + 1)
+    
+    return dfs(root, 0)
