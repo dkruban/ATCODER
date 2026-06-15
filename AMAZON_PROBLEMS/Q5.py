263316bdd1d5bd86cf2f286283e2c67f7d472f2b
Ruban.M <dkrkumar8585@gmail.com>
2026-06-15 12:34:39 +0530

    Create Q5.py

---
+++ AMAZON_PROBLEMS/Q5.py
@@ -0,0 +1,23 @@
+def search_rotated(A, N, key):
+    low, high = 0, N - 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        
+        if A[mid] == key:
+            return mid
+        
+        # Left half is sorted
+        if A[low] <= A[mid]:
+            if A[low] <= key < A[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+        # Right half is sorted
+        else:
+            if A[mid] < key <= A[high]:
+                low = mid + 1
+            else:
+                high = mid - 1
+                
+    return -1
