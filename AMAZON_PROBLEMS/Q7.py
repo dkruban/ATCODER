d5607d707f0ab29917593d9ce663962ab6954f44
Ruban.M <dkrkumar8585@gmail.com>
2026-06-15 12:35:18 +0530

    Create Q7.py

---
+++ AMAZON_PROBLEMS/Q7.py
@@ -0,0 +1,27 @@
+def kadane(arr):
+    max_so_far = float('-inf')
+    current_max = 0
+    for x in arr:
+        current_max += x
+        if max_so_far < current_max:
+            max_so_far = current_max
+        if current_max < 0:
+            current_max = 0
+    return max_so_far if max_so_far > 0 else max(arr) # Handle all negatives
+
+def max_sum_submatrix(M, R, C):
+    max_sum = float('-inf')
+    
+    for left in range(C):
+        temp = [0] * R
+        for right in range(left, C):
+            # Add column values to temp
+            for i in range(R):
+                temp[i] += M[i][right]
+            
+            # Apply Kadane's on temp
+            current_max = kadane(temp)
+            if current_max > max_sum:
+                max_sum = current_max
+                
+    return max_sum
