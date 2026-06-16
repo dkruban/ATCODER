f1469960a3bbd7478e4300734ddf35eeb71a8e0a
Ruban.M <dkrkumar8585@gmail.com>
2026-06-16 10:30:55 +0530

    Update frog2.py

--- ATCODE/frog2.py
+++ ATCODE/frog2.py
@@ -1,7 +1,6 @@
 import sys
 
 def main():
-    # Read all input from standard input
     input = sys.stdin.read
     data = input().split()
     
@@ -11,7 +10,6 @@ def main():
     n = int(data[0])
     h = [int(x) for x in data[1:]]
     
-    # dp[i] will store the minimum cost to reach stone i
     dp = [0] * n
     
     # Base cases
@@ -19,17 +17,11 @@ def main():
     if n > 1:
         dp[1] = abs(h[1] - h[0])
         
-    # Fill the DP table for the remaining stones
     for i in range(2, n):
-        # Option 1: Jump from the previous stone (i-1)
         jump_one = dp[i-1] + abs(h[i] - h[i-1])
-        # Option 2: Jump from two stones back (i-2)
         jump_two = dp[i-2] + abs(h[i] - h[i-2])
-        
-        # Take the minimum of both choices
         dp[i] = min(jump_one, jump_two)
         
-    # The answer is the cost to reach the last stone (N-1 in 0-indexed)
     print(dp[n-1])
 
 if __name__ == '__main__':
