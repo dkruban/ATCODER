b0a57955070e7c0bf9c36a6f10473ce6a835ee00
Ruban.M <dkrkumar8585@gmail.com>
2026-06-17 22:37:28 +0530

    Update Tatar TV Show.py

--- day3/AF/Tatar TV Show.py
+++ day3/AF/Tatar TV Show.py
@@ -1,7 +1,6 @@
 import sys
 
 def solve():
-    # Fast I/O
     input = sys.stdin.read
     data = input().split()
     
@@ -17,14 +16,11 @@ def solve():
         k = int(data[idx+1])
         s = data[idx+2]
         idx += 3
-        
-        # Track the count of '1's for each remainder modulo k
         ones_count = [0] * k
         for i in range(n):
             if s[i] == '1':
                 ones_count[i % k] += 1
         
-        # Check if every track has an even number of '1's
         possible = True
         for count in ones_count:
             if count % 2 != 0:
